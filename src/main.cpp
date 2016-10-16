1cc51a7fb41fcc757cbdf456c4911d68dc24e09a
Kevin Tong <kcr1230@hotmail.com>
2016-10-16 03:15:34 -0700

    Sorts

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,92 @@
 
 using namespace std;
 
-int main() {
+void arrayCreation(int array[], int n) {
+  for (auto i = 0; i < n; i++) {
+    array[i] = rand() % 23;
+  }
+}
 
-    for (int i = 1; i <= 100; i++) {
-        if (!(i % 3) && !(i % 5)) {
-            cout << "FizzBuzz" << endl;
-        } else if (!(i % 3)) {
-            cout << "Fizz" << endl;
-        } else if (!(i % 5)) {
-            cout << "Buzz" << endl;
-        } else {
-            cout << i << endl;
-        }
+void selectionSort(int array[], int n) {
+  auto i = 0;
+  auto j = 0;
+  auto minIndex = 0;
+  auto temp = 0;
+
+  for (i = 0; i < n - 1; i++) {
+    minIndex = i;
+    for (j = i + 1; j < n; j++)
+      if (array[j] < array[minIndex])
+        minIndex = j;
+    if (minIndex != i) {
+      temp = array[i];
+      array[i] = array[minIndex];
+      array[minIndex] = temp;
     }
+  }
+}
 
-    return 0;
+void insertionSort(int array[], int n) {
+  for (auto i = 1; i < n; i++) {
+    auto current = array[i];
+    int j;
+    for (j = i - 1; j >= 0 && current < array[j]; j--) {
+      array[j + 1] = array[j];
+    }
+    array[j + 1] = current;
+  }
+}
 
+void selectionSortExecution() {
+  const auto arraySize = 5;
+  int array[arraySize];
+
+  arrayCreation(array, arraySize);
+
+  for (auto i : array) {
+    cout << i << " ";
+  }
+
+  cout << endl;
+
+  selectionSort(array, arraySize);
+
+  for (auto i : array) {
+    cout << i << " ";
+  }
+
+  cout << endl;
+}
+
+void insertionSortExecution() {
+  const auto arraySize = 5;
+  int array[arraySize];
+
+  arrayCreation(array, arraySize);
+
+  for (auto i : array) {
+    cout << i << " ";
+  }
+
+  cout << endl;
+
+  insertionSort(array, arraySize);
+
+  for (auto i : array) {
+    cout << i << " ";
+  }
+
+  cout << endl;
+}
+
+int main() {
+  cout << "Selection Sort:" << endl;
+  selectionSortExecution();
+
+  cout << endl;
+
+  cout << "Insertion Sort:" << endl;
+  insertionSortExecution();
+
+  return 0;
 }
